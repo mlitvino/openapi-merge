2ae2a71e4b6d8b0d38f2a4daaf04d1ed6efac64d
Misha <darkrabbit240@gmail.com>
2026-05-20 23:41:47 +0300

    fix: add safe check for isPromise

--- packages/openapi-merge/src/utils.ts
+++ packages/openapi-merge/src/utils.ts
@@ -8,5 +8,9 @@ export function createContext(): MergeContext {
 }
 
 export function isPromise<T>(value: Promise<T> | T): value is Promise<T> {
-  return typeof (value as Promise<T>).then === 'function';
+  return (
+    (typeof value === 'object' || typeof value === 'function') &&
+    value !== null &&
+    typeof (value as Promise<T>).then === 'function'
+  );
 }
